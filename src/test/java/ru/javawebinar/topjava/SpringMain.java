124a0296a8542ecaafffd03cece1a746e237910e
JohannStolz <shtolz35@gmail.com>
2018-07-30 13:43:19 +0300

    HW5 optional part 6 fix MealServlet and SpringMain are done

--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.to.MealWithExceed;
@@ -17,18 +18,24 @@ import java.util.List;
 public class SpringMain {
     public static void main(String[] args) {
         // java 7 Automatic resource management
-        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/mock.xml")) {
-            System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
-            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-            adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ROLE_ADMIN));
-            System.out.println();
+        try (ConfigurableApplicationContext tunableAppCtx = new ClassPathXmlApplicationContext()) {
+            ConfigurableEnvironment environment = tunableAppCtx.getEnvironment();
+            environment.addActiveProfile(Profiles.DATAJPA);
+            environment.addActiveProfile(Profiles.POSTGRES_DB);
+            tunableAppCtx.refresh();
+            try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/mock.xml")) {
+                System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+                AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
+                adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ROLE_ADMIN));
+                System.out.println();
 
-            MealRestController mealController = appCtx.getBean(MealRestController.class);
-            List<MealWithExceed> filteredMealsWithExceeded =
-                    mealController.getBetween(
-                            LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
-                            LocalDate.of(2015, Month.MAY, 31), LocalTime.of(11, 0));
-            filteredMealsWithExceeded.forEach(System.out::println);
+                MealRestController mealController = appCtx.getBean(MealRestController.class);
+                List<MealWithExceed> filteredMealsWithExceeded =
+                        mealController.getBetween(
+                                LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
+                                LocalDate.of(2015, Month.MAY, 31), LocalTime.of(11, 0));
+                filteredMealsWithExceeded.forEach(System.out::println);
+            }
         }
     }
 }
