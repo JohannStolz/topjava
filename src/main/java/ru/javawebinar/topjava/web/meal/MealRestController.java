14672d427430ac8657cfcb1e99c57ec23e71a3d5
JohannStolz <shtolz35@gmail.com>
2018-08-10 17:22:06 +0300

    fix SpringMain, MealRestController

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,46 +1,35 @@
 package ru.javawebinar.topjava.web.meal;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.service.MealService;
 import ru.javawebinar.topjava.to.MealWithExceed;
-import ru.javawebinar.topjava.util.DateTimeUtil;
-import ru.javawebinar.topjava.util.MealsUtil;
-import ru.javawebinar.topjava.web.SecurityUtil;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
-
-import static ru.javawebinar.topjava.util.Util.orElse;
-import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
-import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
 
 @Controller
 public class MealRestController extends AbstractMealController {
 
 
     public Meal get(int id) {
-        return get(id);
+        return super.get(id);
     }
 
     public void delete(int id) {
-        delete(id);
+        super.delete(id);
     }
 
     public List<MealWithExceed> getAll() {
-        return getAll();
+        return super.getAll();
     }
 
     public Meal create(Meal meal) {
-        return create(meal);
+        return super.create(meal);
     }
 
     public void update(Meal meal, int id) {
-        update(meal, id);
+        super.update(meal, id);
     }
 
     /**
@@ -50,6 +39,6 @@ public class MealRestController extends AbstractMealController {
      * </ol>
      */
     public List<MealWithExceed> getBetween(LocalDate startDate, LocalTime startTime, LocalDate endDate, LocalTime endTime) {
-        return getBetween(startDate, startTime, endDate, endTime);
+        return super.getBetween(startDate, startTime, endDate, endTime);
     }
 }
