c4b07e901a9f429edd53470e658b01cff0889f8b
JohannStolz <shtolz35@gmail.com>
2018-09-04 11:01:56 +0300

    HW9_2 adapt to work meals.jsp by ajax done

--- src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
@@ -1,6 +1,5 @@
 package ru.javawebinar.topjava.web.meal;
 
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,10 +13,8 @@ import ru.javawebinar.topjava.util.ResponseEntityErrorHandler;
 
 import javax.validation.Valid;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
-import java.util.StringJoiner;
 
 @RestController
 @RequestMapping(value = "/ajax/profile/meals")
@@ -54,7 +51,7 @@ public class MealAjaxController extends AbstractMealController {
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
-     @Override
+    @Override
     @GetMapping(value = "/filter", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<MealWithExceed> getBetween(
             @RequestParam(value = "startDate", required = false) LocalDate startDate,
--- src/main/webapp/resources/js/mealDatatables.js
+++ src/main/webapp/resources/js/mealDatatables.js
@@ -24,7 +24,16 @@ $(function () {
         "info": true,
         "columns": [
             {
-                "data": "dateTime"
+                "data": "dateTime",
+                "render": function (data, type, row) {
+                    return new Date(data).toLocaleString([], {
+                        year: 'numeric',
+                        month: '2-digit',
+                        day: '2-digit',
+                        hour: '2-digit',
+                        minute: '2-digit'
+                    });
+                }
             },
             {
                 "data": "description"
