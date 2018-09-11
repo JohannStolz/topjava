e4416934700d9e833bf45cf848d7d9b4fcf3a56b
JohannStolz <shtolz35@gmail.com>
2018-09-11 18:51:03 +0300

    HW10_3 make error handling when duplicating email done.

--- src/main/java/ru/javawebinar/topjava/web/ExceptionInfoHandler.java
+++ src/main/java/ru/javawebinar/topjava/web/ExceptionInfoHandler.java
@@ -2,6 +2,9 @@ package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -30,7 +33,8 @@ import static ru.javawebinar.topjava.util.exception.ErrorType.*;
 @Order(Ordered.HIGHEST_PRECEDENCE + 5)
 public class ExceptionInfoHandler {
     private static Logger log = LoggerFactory.getLogger(ExceptionInfoHandler.class);
-
+    @Autowired
+    private ReloadableResourceBundleMessageSource bundleMessageSource;
     //  http://stackoverflow.com/a/22358422/548473
     @ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)
     @ExceptionHandler(NotFoundException.class)
@@ -63,7 +67,7 @@ public class ExceptionInfoHandler {
     }
 
     //    https://stackoverflow.com/questions/538870/should-private-helper-methods-be-static-if-they-can-be-static
-    private static ErrorInfo logAndGetErrorInfo(HttpServletRequest req, Exception e, boolean logException, ErrorType errorType) {
+    private ErrorInfo logAndGetErrorInfo(HttpServletRequest req, Exception e, boolean logException, ErrorType errorType) {
         Throwable rootCause = ValidationUtil.getRootCause(e);
         if (logException) {
             log.error(errorType + " at request " + req.getRequestURL(), rootCause);
@@ -75,6 +79,8 @@ public class ExceptionInfoHandler {
             message = getErrorMessage(((BindException) e).getBindingResult());
         } else if (e instanceof MethodArgumentNotValidException) {
             message = getErrorMessage(((MethodArgumentNotValidException) e).getBindingResult());
+        } else if (e instanceof DataIntegrityViolationException && rootCause.toString().contains("users_unique_email_idx")) {
+            message = bundleMessageSource.getMessage("user.unique_email_error", null, LocaleContextHolder.getLocale());
         }
         return new ErrorInfo(req.getRequestURL(), errorType, message);
     }
